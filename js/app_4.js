5f513a5a07f9ba0992622ca5d7a17c7b4c5431c0
JCalc46 <JCalc46@users.noreply.github.com>
2014-11-13 15:12:43 -0600

    Update app_4.js

--- js/app_4.js
+++ js/app_4.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'jcalc46.k7c238fj'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1IjoiamNhbGM0NiIsImEiOiItckdIc0hJIn0.H1kjwmZbTZEMX1jgp2FloA'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
@@ -14,7 +14,7 @@ map.setView([39, -96], 4);
 ///////////////////////////////////////////////////////////////////////////
 // This is the area we're going to use to add data to our map
 
-var dataFileToAdd = 'data/powercat.geojson'; //<- Point this to the file that you want to include on the map
+var dataFileToAdd = 'data/CylceBoulder.geojson'; //<- Point this to the file that you want to include on the map
 var dataToAdd;
 
 var featureLayer = L.mapbox.featureLayer()
@@ -59,7 +59,7 @@ var clickHandler = function(e){
   $('#info').fadeIn(400,function(){
     var info = '';
 
-    info = '<div>You clicked on the layer with the id of ' + feature.properties.id + '</div>';
+    info = '<div>You clicked on the layer with the id of ' + feature.properties.surface + '</div>';
 
     $('#info').append(info);
   });
