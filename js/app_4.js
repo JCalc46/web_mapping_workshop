59f2c8432c61b1ede5ae5d072e10a124e889a14d
JCalc46 <JCalc46@users.noreply.github.com>
2014-11-13 15:35:07 -0600

    Update app_4.js

--- js/app_4.js
+++ js/app_4.js
@@ -71,3 +71,37 @@ featureLayer.on('ready', function(){
     layer.on('click', clickHandler);
   });
 });
+var myLocation = L.mapbox.featureLayer().addTo(map);
+
+map.locate();
+
+map.on('locationfound', function(e) {
+
+	
+myLocation.setGeoJSON({
+
+type: 'Feature',
+
+geometry: {
+
+type: 'Point',
+
+coordinates: [e.latlng.lng, e.latlng.lat]
+
+},
+
+properties: {
+
+'title': 'Here I am!',
+
+'marker-color': '#ff8888',
+
+'marker-symbol': 'star'
+
+}
+
+});
+
+});
+
+map.locate();
